9128d220d6fd9546f7b0d48209bf14be3cb6af8c
Zhicheng Cai <yzzsjc1234@gmail.com>
2017-07-11 15:12:10 +0800

    JavaEE web project initialization and built customer-decidedzone association function

---
+++ src/cn/homedelivery/crm/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,58 @@
+package cn.homedelivery.crm.service.impl;
+
+import java.util.List;
+
+import org.hibernate.Session;
+
+import cn.homedelivery.crm.domain.Customer;
+import cn.homedelivery.crm.service.CustomerService;
+import cn.homedelivery.crm.utils.HibernateUtils;
+
+public class CustomerServiceImpl implements CustomerService {
+
+	public List<Customer> findnoassociationCustomers() {
+		Session session = HibernateUtils.openSession();
+		session.beginTransaction();
+
+		String hql = "from Customer where decidedzone_id is null";
+		List<Customer> customers = session.createQuery(hql).list();
+
+		session.getTransaction().commit();
+		session.close();
+
+		return customers;
+	}
+
+	public List<Customer> findhasassociationCustomers(String decidedZoneId) {
+		Session session = HibernateUtils.openSession();
+		session.beginTransaction();
+
+		String hql = "from Customer where decidedzone_id = ?";
+		List<Customer> customers = session.createQuery(hql).setParameter(0, decidedZoneId).list();
+
+		session.getTransaction().commit();
+		session.close();
+
+		return customers;
+	}
+
+	public void assignCustomersToDecidedZone(Integer[] customerIds, String decidedZoneId) {
+		Session session = HibernateUtils.openSession();
+		session.beginTransaction();
+
+		// Cancel all associated customers of the decidedzone
+		String hql2 = "update Customer set decidedzone_id=null where decidedzone_id=?";
+		session.createQuery(hql2).setParameter(0, decidedZoneId).executeUpdate();
+
+		// To do association
+		String hql = "update Customer set decidedzone_id=? where id =?";
+		if (customerIds != null) {
+			for (Integer id : customerIds) {
+				session.createQuery(hql).setParameter(0, decidedZoneId).setParameter(1, id).executeUpdate();
+			}
+		}
+		session.getTransaction().commit();
+		session.close();
+	}
+
+}
---
+++ src/cn/homedelivery/crm/utils/HibernateUtils.java
@@ -0,0 +1,26 @@
+package cn.homedelivery.crm.utils;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hibernate tool
+ * 
+ */
+public class HibernateUtils {
+	private static Configuration configuration;
+	private static SessionFactory sessionFactory;
+
+	static {
+		configuration = new Configuration().configure();
+		sessionFactory = configuration.buildSessionFactory();
+	}
+
+	public static Session openSession() {
+		return sessionFactory.openSession();
+	}
+
+	public static void main(String[] args) {
+	}
+}
